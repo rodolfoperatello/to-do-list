7415b184858685a9c90cc0915e5a707f20baa8db
rodolfoperatello <rodolfoperatello@hotmail.com>
2021-02-11 21:59:58 -0300

    Creating UserRequest, UserResponse and TaskMapper class

---
+++ src/main/java/br/com/rodolfoperatello/todolist/controller/request/UserRequest.java
@@ -0,0 +1,2 @@
+package br.com.rodolfoperatello.todolist.controller.request;public class UserRequest {
+}
---
+++ src/main/java/br/com/rodolfoperatello/todolist/controller/response/UserResponse.java
@@ -0,0 +1,4 @@
+package br.com.rodolfoperatello.todolist.controller.response;
+
+public class UserResponse {
+}
