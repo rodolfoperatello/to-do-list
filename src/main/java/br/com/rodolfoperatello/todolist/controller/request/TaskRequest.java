094634a19789ab38c4c7c4fdc0da5fe31d751fd3
rodolfoperatello <rodolfoperatello@hotmail.com>
2021-02-15 10:26:24 -0300

    Removing unnecessary imports

--- src/main/java/br/com/rodolfoperatello/todolist/controller/request/TaskRequest.java
+++ src/main/java/br/com/rodolfoperatello/todolist/controller/request/TaskRequest.java
@@ -3,7 +3,6 @@ package br.com.rodolfoperatello.todolist.controller.request;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 public class TaskRequest {
 
